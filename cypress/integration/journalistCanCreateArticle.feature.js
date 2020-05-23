152a30dce7ca936096dc1f8feac12bb7ad957fbe
allezz <aerbay@gmail.com>
2020-05-23 18:36:40 +0200

    add message field for response,add start-server-and-test package

--- cypress/integration/journalistCanCreateArticle.feature.js
+++ cypress/integration/journalistCanCreateArticle.feature.js
@@ -1,30 +1,34 @@
-describe('Journalist can create an article', () => {
+describe("Journalist can create an article", () => {
   beforeEach(() => {
-    cy.server()
+    cy.server();
     cy.route({
       method: "POST",
       url: "http://localhost:3000/api/articles*",
-      response: "fixture:success_message.json"
-    })
-    cy.visit("/")
-  })
+      response: "fixture:success_message.json",
+    });
+    cy.visit("/");
+  });
 
-  it('successfully with title and body', () => {
-    cy.get('input#title').type('This is the title')
-    cy.get('textarea#body').type('This is the body this is the body this is the body this is the body this is the body.')
-    cy.get('#post').click()
-    cy.get('#message').should('contain', 'Article successfully created!')
-  })
+  it("successfully with title and body", () => {
+    cy.get("input#title").type("This is the title");
+    cy.get("textarea#body").type(
+      "This is the body this is the body this is the body this is the body this is the body."
+    );
+    cy.get("#post").click();
+    cy.get("#message").should("contain", "Article successfully created!");
+  });
 
-  it('unsuccessfully without entering any title', () => {
-    cy.get('input#body').type('This is the body this is the body this is the body this is the body this is the body.')
-    cy.get('#post').click()
-    cy.get('#message').should('contain', "Title can't be blank")
-  })
+  it("unsuccessfully without entering any title", () => {
+    cy.get("textarea#body").type(
+      "This is the body this is the body this is the body this is the body this is the body."
+    );
+    cy.get("#post").click();
+    cy.get("#message").should("not.be.visible");
+  });
 
-  it('unsuccessfully without entering any body text', () => {
-    cy.get('input#title').type('This is the title')
-    cy.get('#post').click()
-    cy.get('#message').should('contain', "Body can't be blank")
-  })
-})
+  it("unsuccessfully without entering any body text", () => {
+    cy.get("input#title").type("This is the title");
+    cy.get("#post").click();
+    cy.get("#message").should("not.be.visible");
+  });
+});
--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -1,35 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Input } from "semantic-ui-react";
-import axios from 'axios'
+import axios from "axios";
 
 const CreateArticle = () => {
+  const [message, setMessage] = useState("");
+
   const onSubmitHandler = async (e) => {
-
     try {
-      const response = await axios.post('/articles', {
-        params: { title: e.target.title.value, body: e.target.body.value }
-      })
-      debugger
+      const response = await axios.post("/articles", {
+        params: { title: e.target.title.value, body: e.target.body.value },
+      });
+      setMessage(response.data.message);
     } catch (error) {
-      debugger
+      setMessage(error.response.data.message);
     }
-  }
-
+  };
 
   return (
     <div>
       <Form onSubmit={onSubmitHandler}>
         <Form.Field>
           <label>Title</label>
-          <Input id="title" name="title" placeholder="Title" />
+          <Input id="title" name="title" placeholder="Title" required />
         </Form.Field>
         <Form.Field>
           <label>Content</label>
-          <textarea id="body" name="body" placeholder="Content" />
+          <textarea id="body" name="body" placeholder="Content" required />
         </Form.Field>
         <Button id="post" type="submit">
           Post
         </Button>
+        <p id="message">{message}</p>
       </Form>
     </div>
   );
