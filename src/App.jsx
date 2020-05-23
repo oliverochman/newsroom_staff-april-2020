47f42ae9ad57e40a09550a632db51ee068c65cdd
pauline_barnades <barnades.pauline@gmail.com>
2020-05-23 14:47:39 +0200

    CreateArticle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import CreateArticle from './components/CreateArticle'
+
 
 function App() {
   return (
     <div className="App">
       Daily News Sense
+       <CreateArticle/>
     </div>
   );
 }
