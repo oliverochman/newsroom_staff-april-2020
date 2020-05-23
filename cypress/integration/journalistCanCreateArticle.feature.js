4dd5444e92111220c471caea99987233068d4960
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-23 13:06:32 +0200

    writing feature test for creating article, including fixture from api documentation

---
+++ cypress/integration/journalistCanCreateArticle.feature.js
@@ -0,0 +1,30 @@
+describe('Journalist can create an article', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: "POST",
+      url: "/articles",
+      response: "fixture:success_message.json"
+    })
+    cy.visit("/")
+  })
+
+  it('successfully with title and body', () => {
+    cy.get('input#title').type('This is the title')
+    cy.get('input#body').type('This is the body this is the body this is the body this is the body this is the body.')
+    cy.get('#post').click()
+    cy.get('#message').should('contain', 'Article successfully created!')
+  })
+
+  it('unsuccessfully without entering any title', () => {
+    cy.get('input#body').type('This is the body this is the body this is the body this is the body this is the body.')
+    cy.get('#post').click()
+    cy.get('#message').should('contain', "Title can't be blank")
+  })
+
+  it('unsuccessfully without entering any body text', () => {
+    cy.get('input#title').type('This is the title')
+    cy.get('#post').click()
+    cy.get('#message').should('contain', "Body can't be blank")
+  })
+})
