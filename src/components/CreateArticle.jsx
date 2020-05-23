a5d450f148a9ed1de940ebd44de4d0b67081a73e
pauline_barnades <barnades.pauline@gmail.com>
2020-05-23 14:49:15 +0200

    adding form to component

--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -1,1 +1,23 @@
+import React from 'react'
+import { Form, Button } from "semantic-ui-react"
 
+const CreateArticle = () => {
+  
+  return (
+    <div>
+      <Form>
+      <Form.Field>
+        <label>Title</label>
+          <input placeholder='Title'/>
+        </Form.Field>
+        <Form.Field>
+          <label>Content</label>
+          <input placeholder='Content'/>
+        </Form.Field>
+          <Button type='submit'>Submit</Button>
+    </Form>
+    </div>
+  )
+}
+
+export default CreateArticle
