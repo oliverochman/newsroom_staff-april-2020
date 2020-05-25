2772d2e682b3ee2688519b4570a4d9e6c9f96406
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-25 18:33:20 +0200

    adding test for logout and redirect back to login page

--- cypress/integration/journalistCanLogin.feature.js
+++ cypress/integration/journalistCanLogin.feature.js
@@ -61,4 +61,52 @@ describe('Journalist authenticates', () => {
       cy.get("#error-message").should("contain", "Invalid login credentials. Please try again.");
     });
   })
+
+  describe('and can end his/her session', () => {
+    beforeEach(() => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/auth/*",
+        response: "fixture:successful_login.json",
+        headers: {
+          uid:"user@mail.com"
+        }
+      })
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/auth/*",
+        response: "fixture:successful_login.json",
+        headers: {
+          uid:"user@mail.com"
+        }
+      })
+      cy.route({
+        method: "DELETE",
+        url: "http://localhost:3000/api/auth*",
+        response: "fixture:logout.json",
+        headers: {
+          uid: "user@mail.com"
+        }
+      })
+      cy.get('#login-form').within(() => {
+        cy.get('#email').type('user@mail.com');
+        cy.get('#password').type('password');
+        cy.get('Button').contains('Submit').click();
+      });
+    })
+
+    it('clicking the Log out button', () => {
+      cy.get('.header').within(() => {
+        cy.get('button').contains('Log out user@mail.com').click()
+        cy.get('button').contains('Log out user@mail.com').should('not.exist')
+      })
+    })
+
+    it('and is redirected to login page', () => {
+      cy.get('.header').within(() => {
+        cy.get('button').contains('Log out user@mail.com').click()
+      })
+      cy.get('#login-form').should('be.visible')
+    })
+  })
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import { Grid, Button } from "semantic-ui-react";
+import auth from '../modules/auth'
+import { Redirect } from 'react-router-dom'
 
 const Header = (props) => {
+  const logOut = async () => {
+    try {
+      await auth.signOut()
+      props.setAuthenticated(false)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const redirect = !props.authenticated && <Redirect to={{pathname: '/'}}/>
 
   return (
-    <Grid className="header" padded>
-      <Grid.Column width={5}>
-        <h1 id="header">Daily News Sense</h1>
-      </Grid.Column>
-      <Grid.Row celled columns='equal'>
-        <Grid.Column>
-          Write
+    <>
+      {redirect}
+      <Grid className="header" padded>
+        <Grid.Column width={5}>
+          <h1 id="header">Daily News Sense</h1>
         </Grid.Column>
-        <Grid.Column width={4}>
-          {props.authenticated && (
-            <Button basic inverted>Log out {props.uid}</Button>
-          )}
-        </Grid.Column>
-      </Grid.Row>
+        <Grid.Row celled columns='equal'>
+          <Grid.Column>
+            Write
+          </Grid.Column>
+          <Grid.Column width={4}>
+            {props.authenticated && (
+              <Button basic inverted onClick={() => logOut()}>Log out {props.uid}</Button>
+              )}
+          </Grid.Column>
+        </Grid.Row>
     </Grid>
+    </>
   );
 };
 export default Header;
