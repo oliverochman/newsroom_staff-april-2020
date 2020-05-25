10e30e25ee0ee2fa410d947d97d7dd824b1bcdd3
pauline_barnades <barnades.pauline@gmail.com>
2020-05-25 18:02:29 +0200

    Editing component, adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+import LoginForm from './components/LoginForm';
 import CreateArticle from './components/CreateArticle'
-
+import Header from './components/Header'
+import { Switch, Route } from 'react-router-dom'
+import auth from "./modules/auth"
 
 function App() {
+  const [uid, setUid] = useState("")
+  const [authenticated, setAuthenticated] = useState(false)
+
+  useEffect(() => {
+    async function validate() {
+      if (localStorage.hasOwnProperty("J-tockAuth-Storage")) {
+        const tokenParams = JSON.parse(localStorage.getItem("J-tockAuth-Storage"))
+        try {
+          const response = await auth.validateToken(tokenParams)
+          setAuthenticated(response.success)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    validate();
+  },[])
+
   return (
     <div className="App">
-      <Header/>
-      <CreateArticle/>
+      <Header uid={uid} authenticated={authenticated} setAuthenticated={setAuthenticated}/>
+      <Switch>
+        <Route exact path="/" render={() => <LoginForm setAuthenticated={setAuthenticated} setUid={setUid} authenticated={authenticated}/>}  />
+        <Route authenticated={authenticated} path='/write'component={CreateArticle} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 import './css/index.css';
 
@@ -10,7 +11,9 @@ axios.defaults.baseURL = "http://localhost:3000/api"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
