c12e9fb5abde9a5cf4901c83385a3a981cbbdae5
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-27 21:59:57 +0200

    adding fixture, finishing writing tests

--- cypress/integration/editorCanSeeUnpublishedArticles.feature.js
+++ cypress/integration/editorCanSeeUnpublishedArticles.feature.js
@@ -1,12 +1,44 @@
 describe("Editor can see unpublished articles", () => {
-  beforeEach(() => {
-    cy.login();
-  });
+  describe('when logged in as editor', () => {
+    beforeEach(() => {
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/admin/articles",
+        response: "fixture:unpublished_article_list.json",
+      });
+      cy.login('editor');
+      cy.get("#review-nav").click();
+    });
 
-  cy.get("#review-nav").click();
+    it("articles is shown", () => {
+      cy.get("#article-1").should("contain", "title 1");
+      cy.get("#article-2").should("contain", "title 2");
+    });
 
-  it("articles is shown", () => {
-    cy.get("#article-1").should("contain", "title 1");
-    cy.get("#article-2").should("contain", "title 2");
-  });
+    it("article details are shown", () => {
+      cy.get('#article-1').within((block) => {
+        block.should('contain', 'Created at: ')
+        block.should('contain', 'Category:')
+      })
+    })
+
+    it('on the /review page', () => {
+      cy.url().should('contain', '/review')
+    })
+  })
+
+  describe('but logged in journalists cannot', () => {
+    beforeEach(() => {
+      cy.login('journalist');
+    });
+
+    it('and cannot see the nav-button', () => {
+      cy.get('#review-nav').should('not.exist')
+    })
+
+    it('and is redirected when going to page directly', () => {
+      cy.visit('/review')
+      cy.url().should('not.contain', '/review')
+    })
+  })
 });
