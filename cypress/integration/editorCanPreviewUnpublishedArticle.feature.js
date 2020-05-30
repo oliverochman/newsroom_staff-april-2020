ce776115e7efb7cd4c727c07f9417e410f23b09d
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-30 16:29:13 +0200

    adding small test and fixtures

---
+++ cypress/integration/editorCanPreviewUnpublishedArticle.feature.js
@@ -0,0 +1,22 @@
+describe("Editor can see unpublished articles", () => {
+  describe("when logged in as editor", () => {
+    beforeEach(() => {
+      cy.login("editor");
+      cy.visit('/review')
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/admin/articles/1",
+        response: "fixture:single_unpublished_article.json",
+      });
+    });
+
+    it('can click and view an article on the review page',() => {
+      cy.get('#article-1').click()
+      cy.get('#preview').within(() => {
+        cy.get('#title').should('contain', "title 1")
+        cy.get('#body').should('contain', "Lorem ipsum")
+        cy.get('#image').should('be.visible')
+      })
+    })
+  })
+})
